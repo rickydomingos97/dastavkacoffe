7a5e8328b1405425ad02fbe36dc53b6a5d069be3
rickydomingos97 <ricardodomingos97@gmail.com>
2023-03-15 00:33:31 -0300

    complete order form update width 90rem
    update complete order page width 90rem update

--- src/pages/CompleteOrder/styles.ts
+++ src/pages/CompleteOrder/styles.ts
@@ -5,6 +5,7 @@ export const CompleteOrderContainer = styled.form`
   display: flex;
   justify-content: space-between;
   gap: 2rem;
+  max-width: 90rem;
 `
 
 export const SectionBaseStyle = styled.div`
